b5d7676cb861829cae87a49ce9f23152464b102e
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-08 21:19:51 -0500

    commit tree 30fd078e85b5c638798ab33a14ab6b2d13f0d86e

--- js/reducer.js
+++ js/reducer.js
@@ -1,19 +1,33 @@
-let state = {count: 0};
+function changeState(state = { count: 0 }, action) {
 
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+  switch (action.type) {
+
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 }
+
+    default:
+      return state;
   }
+} 
 
-function dispatch(action){
-    state = changeState(state, action)
-    render()
-}
+// let state;
 
-function render(){
-    document.body.textContent = state.count
-}
+// function changeState(state, action){
+//     switch (action.type) {
+//       case 'INCREASE_COUNT':
+//         return {count: state.count + 1}
+//       default:
+//         return state;
+//     }
+//   }
+
+// function dispatch(action){
+//     state = changeState(state, action)
+//     render()
+// }
+
+// function render(){
+//     document.body.textContent = state.count
+// }
+
+// dispatch({ type: '@@INIT' })
